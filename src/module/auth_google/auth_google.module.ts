6385fc628416f0293e78ab30b545698a21856b79
abbosxon <abbosxon.oripov@yandex.ru>
2023-03-19 23:21:04 +0500

    fix some error in importing middleware

--- src/module/auth_google/auth_google.module.ts
+++ src/module/auth_google/auth_google.module.ts
@@ -4,14 +4,15 @@ import {JwtModule, JwtService} from '@nestjs/jwt'
 import { GoogleLoginStrategy } from './strategy/google_login.strategy';
 import { AuthGoogleService } from './auth_google.service';
 import { Module } from '@nestjs/common';
+import { TokenMiddleware } from 'src/middleWare/token.middleware';
 
 @Module({
   imports: [
     JwtModule.register({
       secret: process.env.SECRET_KEY
-    })
+    }),
   ],
   controllers: [AuthGoogleController],
-  providers: [AuthGoogleService, GoogleRegisterStrategy, GoogleLoginStrategy, JwtService]
+  providers: [AuthGoogleService, GoogleRegisterStrategy, GoogleLoginStrategy, JwtService, TokenMiddleware]
 })
 export class AuthGoogleModule {}
