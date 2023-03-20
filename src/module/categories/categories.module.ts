1f063c3b05546185dd9de1f0e9892d173bf4040e
simlenodir <nodirsmailov0@gmail.com>
2023-03-20 22:56:57 +0300

    category crud

--- src/module/categories/categories.module.ts
+++ src/module/categories/categories.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
+import { TokenMiddleware } from 'src/middleWare/token.middleware';
 
 @Module({
   controllers: [CategoriesController],
-  providers: [CategoriesService]
+  providers: [CategoriesService, TokenMiddleware]
 })
 export class CategoriesModule {}
