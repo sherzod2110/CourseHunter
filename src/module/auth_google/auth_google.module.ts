671703ab2cf7fc5e110bce6c66fcb520f21495b2
abbosxon <abbosxon.oripov@yandex.ru>
2023-03-21 09:21:04 +0500

    simple changes +

--- src/module/auth_google/auth_google.module.ts
+++ src/module/auth_google/auth_google.module.ts
@@ -1,10 +1,10 @@
 import { GoogleRegisterStrategy } from './strategy/google_register.strategy';
+import { JwtModule, JwtService } from '@nestjs/jwt'
 import { AuthGoogleController } from './auth_google.controller';
-import {JwtModule, JwtService} from '@nestjs/jwt'
 import { GoogleLoginStrategy } from './strategy/google_login.strategy';
 import { AuthGoogleService } from './auth_google.service';
-import { Module } from '@nestjs/common';
 import { TokenMiddleware } from 'src/middleWare/token.middleware';
+import { Module } from '@nestjs/common';
 
 @Module({
   imports: [
